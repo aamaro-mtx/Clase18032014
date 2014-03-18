42acc7312148d5ea0327cb636093f34259b8595e
Abel Amaro Julian <amarotux@gmail.com>
2014-03-18 11:46:09 -0600

    Initial Commit

---
+++ Cadenas/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadenas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You shall never \nwalk alone");
+            
+            Console.WriteLine("Dame una cadena :");
+            string cadena = Console.ReadLine();
+            int numCar = cadena.Length;
+            if (numCar < 1)
+            {
+                Console.WriteLine("La cadena debe de tener al menos un caracter");
+                return;
+            }
+            StringBuilder sb = new StringBuilder();
+            int index = 0;
+            do
+            {
+                char actual = cadena[index];
+
+                char anterior = cadena[index - 1];
+                if (actual != ' ')
+                {
+                    sb.Append(cadena[index]);
+                }
+                else if (anterior == ' ')
+                {
+                    sb.Append(cadena[index]);
+                }
+                index++;
+
+            } while (index != numCar);
+            Console.WriteLine("Resultado {0}", sb);
+
+            Console.WriteLine("Presione <enter> para continuar");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ ToUpper/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.CodeDom;
+using System.Text;
+
+namespace ToUpper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //entrada
+            string input;
+            int index = 0, contador = 0;
+
+            //proceso
+            int valCarAct;
+
+            //salida 
+            StringBuilder sb = new StringBuilder();
+            string stringFinal = "";
+
+            //10 Escribe “Dame una cadena”
+            Console.WriteLine("Dame una cadena");
+
+            //20 Leer input
+            input = Console.ReadLine();
+        L30:
+            //30 si index < input.Length
+            if (index < input.Length)
+            {
+                // 40 hacer ctmp= input[index]
+                char ctmp = input[index];
+            Lreduce:
+                //41 Si contador < 32
+                if (contador < 32)
+                {
+                    //42 ctmp= ctmp - 1;
+                    ctmp--;
+                    //43 hacer contador = contador + 1;
+                    contador = contador + 1;
+                    goto Lreduce;
+                }
+                //43 hacer contador = 0
+                contador = 0;
+                //50	resultado = resultado + actual
+                sb.Append(ctmp);
+
+                //60	index = index + 1
+                index = index + 1;
+                //70	Ir a L30
+                goto L30;
+            }
+            
+
+            //#region Conversion de numero a ASCII
+            ////71 index = 0
+            //index = 0;
+
+            //stringFinal = sb.ToString();
+            //char cDec;
+            //if (stringFinal[index] != ',')
+            //{
+
+            //}
+            //#endregion
+
+            //80 Escribe “La cadena resultante es ” Escribe resultado
+            Console.WriteLine(sb.ToString());
+
+            Console.WriteLine("Presione <enter> para continuar");
+            Console.ReadLine();
+        }
+    }
+}
